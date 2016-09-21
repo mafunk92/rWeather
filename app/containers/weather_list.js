1b766cc07ae8b74eaaa662cb35e4cde000ee8733
Michael Funk <mafunk92@gmail.com>
2016-09-20 20:45:18 -0500

    final - need to fix maps

--- app/containers/weather_list.js
+++ app/containers/weather_list.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import WeatherChart from '../components/weather_chart';
+import CityMap from '../components/city_map';
+
 class WeatherList extends Component {
 
     renderWeather(cityData) {
 
         const name = cityData.city.name;
+        const temp = cityData.list.map(weather => weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+        //const lon = cityData.coord.lon;
+        //const lat = cityData.coord.lat;
+        //<td><CityMap lon={ lon } lat={ lat } /></td>
 
         return(
             <tr key={ name }>
+
                 <td>{ name }</td>
+                <td>
+                    <WeatherChart data={ temp } color="red" units="K" />
+                </td>
+                <td>
+                    <WeatherChart data={ pressure } color="orange" units="hPa" />
+                </td>
+                <td>
+                    <WeatherChart data={ humidity } color="blue" units="%" />
+                </td>
             </tr>
         );
     }
@@ -21,9 +40,9 @@ class WeatherList extends Component {
               <thead>
                 <tr>
                     <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+                    <th>Temperature (K)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
                 </tr>
               </thead>
               <tbody>
